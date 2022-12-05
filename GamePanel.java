109a2cd0782f0b67c50092560febed528ce934e6
JiangMona <91548088+JiangMona@users.noreply.github.com>
2022-12-05 11:18:19 -0500

    Add files via upload

--- GamePanel.java
+++ GamePanel.java
@@ -1,45 +1,19 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-public class GamePanel extends JPanel implements ActionListener{
-	private JButton game;//instructions
-	private JButton instructions;//level
-	private JButton btn3;//exit
-	private JButton exit;//exit
-	private JButton back;//instructions
-	
+public class GamePanel extends JPanel{
+	private StatsP stats;
+	private PlayP play;
 	public GamePanel() {
-		game= new JButton();
-		back = new JButton("back");
-		this.add(back);
-		back.addActionListener(this);
-	}
-	public void paintComponent(Graphics g) {//no double buffering --> double buffering: java creates off screen image and replaces entire screen
-		super.paintComponent(g);;
-		g.setFont(new Font("SansSerif", Font.ITALIC, 30));
-		g.drawString("Game", 150, 150);
+		
+		//set layout
+		stats= new StatsP();
+		play= new PlayP();
+		
+		this.setLayout(new BorderLayout());
+		this.add(stats, BorderLayout.SOUTH);
+		this.add(play, BorderLayout.NORTH);
+		
 		
 	}
-	
-	public void actionPerformed(ActionEvent e) {
-		if(e.getSource()==back) {
-			MainP.cardsL.show(MainP.cont, "menu");
-			//go to game
-			
-		}
-		else if(e.getSource()==instructions) {
-			//go to instructions
-		}
-		else if(e.getSource()==btn3) {
-			
-		}
-		else if(e.getSource()==exit) {
-			JOptionPane.showMessageDialog(null, "Goodbye!");//make icon spinning kirby
-			System.exit(0); 
-		}
-		      
-		
-	}
-	
-
 }
--- StatsP.java
+++ StatsP.java
@@ -1,4 +1,32 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+public class StatsP extends JPanel implements ActionListener{
+	ImageIcon scoreBoard;
+	private JButton back;
+	
+	public StatsP() {
+		scoreBoard= new ImageIcon("scoreBoard.png");
+		JLabel score = new JLabel(scoreBoard);
+		back = new JButton("back");
+		this.add(back);
+		back.addActionListener(this);
+		this.add(score);
+		
+	}
+	public void paintComponent(Graphics g) {//no double buffering --> double buffering: java creates off screen image and replaces entire screen
+		super.paintComponent(g);;
+		g.setFont(new Font("SansSerif", Font.ITALIC, 30));
+		g.drawString("Game", 150, 150);
+		
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==back) {
+			MainP.cardsL.show(MainP.cont, "menu");
+			//go to menu
 
-public class StatsP {
+		}
+	}
 
 }
