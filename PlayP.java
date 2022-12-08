a9c5faeada6ffa946b87359bcf5c8ca04730beba
JiangMona <91548088+JiangMona@users.noreply.github.com>
2022-12-08 09:03:10 -0500

    Add files via upload

--- PlayP.java
+++ PlayP.java
@@ -1,10 +1,65 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-public class PlayP extends JPanel {
-	//add map!!
+public class PlayP extends JPanel implements KeyListener, MouseListener, ActionListener{
+	private static ImageIcon player;
+	private int x, y, mouseX, mouseY;
+	private Timer myTimer;
+	
+	
 	public PlayP() {
+		addKeyListener( this );
+		setBackground(Color.YELLOW);  
+		player = new ImageIcon("kirbyFly.gif");
+		x = 100;
+		y = 300;
 		
+		addMouseListener(this);
+		addKeyListener(this);
+		
+		myTimer = new Timer(120, this); 
+	    myTimer.start();
+		setFocusable(true);
 	}
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		g.drawImage(player.getImage(),x,y,60,40,null);
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource()==myTimer){
+			//x += 3;
+			//y += 3;
+			repaint();
+		}
+	}
+	
+	public void mouseClicked( MouseEvent me ){
+	     
+		 mouseX= me.getX();// x coordinates of mouse clicked
+	     mouseY= me.getY();// y coordinates of mouse clicked
+		 
+	      x=mouseX;     // change x coordinates of the player to mouseX
+	      y=mouseY;
+	      repaint();   // call paintComponent method
+	   }
 
+	public void mousePressed( MouseEvent e ){   }
+	public void mouseReleased( MouseEvent e ){   }
+	public void mouseEntered( MouseEvent e ) {   }
+	public void mouseExited( MouseEvent e )  {   }
+	   
+	public void keyPressed( KeyEvent e ){  
+		if(e.getKeyCode()==65)
+			x -= 10;	
+		else if(KeyEvent.getKeyText(e.getKeyCode()).equals("S"))
+	     	y -= 10;
+		else if(KeyEvent.getKeyText(e.getKeyCode()).equals("W"))
+			y += 10;
+		else if(KeyEvent.getKeyText(e.getKeyCode()).equals("D"))
+			x += 10;	
+		repaint();
+	}
+	
+	public void keyReleased( KeyEvent e ) {   }
 }
--- StatsP.java
+++ StatsP.java
@@ -1,23 +1,31 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-public class StatsP extends JPanel implements ActionListener{
+public class StatsP extends JPanel implements ActionListener, KeyListener{
 	ImageIcon scoreBoard;
 	private JButton back;
 	
 	public StatsP() {
-		scoreBoard= new ImageIcon("scoreBoard.png");
-		JLabel score = new JLabel(scoreBoard);
+		scoreBoard= new ImageIcon("scoreBoard.png");//make image object 
+		JLabel score = new JLabel(scoreBoard);//add to a jlabal
+		/*
 		back = new JButton("back");
-		this.add(back);
+		back.addKeyListener( this );
+		back.setBounds(500, 300, 200, 30);
 		back.addActionListener(this);
-		this.add(score);
+		this.add(back);
+		*/
+		this.add(score);//add labal to panel
+		
+		setBackground(Color.ORANGE);  
+		
 		
 	}
 	public void paintComponent(Graphics g) {//no double buffering --> double buffering: java creates off screen image and replaces entire screen
 		super.paintComponent(g);;
 		g.setFont(new Font("SansSerif", Font.ITALIC, 30));
 		g.drawString("Game", 150, 150);
+		
 		
 	}
 	
