c3d6b17cfd8da087f788ae00b28230a5980b508e
JiangMona <91548088+JiangMona@users.noreply.github.com>
2022-12-09 11:16:07 -0500

    Add files via upload

--- StatsP.java
+++ StatsP.java
@@ -2,31 +2,40 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 public class StatsP extends JPanel implements ActionListener{
-	ImageIcon scoreBoard;
-	private JButton back;
-	
+	private ImageIcon scoreBoard, back;
+	private JButton backbtn;
 	public StatsP() {
-		scoreBoard= new ImageIcon("scoreBoard.png");
-		JLabel score = new JLabel(scoreBoard);
-		back = new JButton("back");
-		this.add(back);
-		back.addActionListener(this);
-		this.add(score);
+		this.setLayout(new BorderLayout());
 		
+		back= new ImageIcon("b.png");
+		backbtn = new JButton(back);
+		makeClearButton(backbtn);
+		add(backbtn, BorderLayout.EAST);
+
+	
 	}
 	public void paintComponent(Graphics g) {//no double buffering --> double buffering: java creates off screen image and replaces entire screen
-		super.paintComponent(g);;
-		g.setFont(new Font("SansSerif", Font.ITALIC, 30));
-		g.drawString("Game", 150, 150);
+		
+		scoreBoard= new ImageIcon("scoreBoard.png");
+		g.drawImage(scoreBoard.getImage(), 0, 0, 600, 150, null);
 		
 	}
 	
 	public void actionPerformed(ActionEvent e) {
-		if(e.getSource()==back) {
+		if(e.getSource()==backbtn) {
 			MainP.cardsL.show(MainP.cont, "menu");
 			//go to menu
 
 		}
+	}
+	public void makeClearButton(JButton b) {
+		//set appearance
+		b.setOpaque(false);
+		b.setContentAreaFilled(false);
+		b.setBorderPainted(false);
+		b.setFocusPainted(false);
+		//add cation listener
+		b.addActionListener(this);
 	}
 
 }
